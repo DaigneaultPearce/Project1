817c8936de27e32780a26613d16d658b1a5c8185
DaigneaultPearce <daigneault.pardopearce.515@my.csun.edu>
2018-03-28 01:32:57 -0700

    Project complete.

--- Project1.java
+++ Project1.java
@@ -2,7 +2,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 public class Project1 {//rename
-
+/*
+ * Project1
+ * Daigneault Pardo Pearce
+ * Comp482
+ * Spring2018
+ * Professor John Noga
+ */
 	public static void main(String[] args) throws IOException { 
 		
 		//Initialize scanner & file.
@@ -11,88 +17,83 @@ public class Project1 {//rename
 		//Pull first number in the file.
 		int numPersons = file.nextInt();
 		
-		//VisualDebug
-		System.out.println("There are " + numPersons +" men and " + numPersons + " women.");
-		
-		//Initialize preference lists,  processed lists and given matching.
+		//Initialize preference lists,  processed lists, given matching by man and calculated matching by woman.
 		int[][] mensPreference = new int[numPersons][numPersons];
 		int[][] mensProcessedList = new int[numPersons][numPersons];
 		int[][] womensPreference = new int[numPersons][numPersons];
-		int[][] womensProcessedList = new int[numPersons][numPersons];//delete l8r
+		int[][] womensProcessedList = new int[numPersons][numPersons];
 		int[] mMatch = new int[numPersons];
 		int[] wMatch = new int[numPersons];
 		
 		//Populate the mens preference lists.
-		System.out.println("Mens preferences");
 		for(int i = 0; i < numPersons; i++) {
 			for(int j = 0; j < numPersons; j++) {
 				mensPreference[i][j] = file.nextInt();
-				System.out.println(mensPreference[i][j]);
 			}
 		}
 		
 		//Populate the womens preference lists.
-		System.out.println("Womens preferences");
 		for(int i = 0; i < numPersons; i++) {
 			for(int j = 0; j < numPersons; j++) {
 				womensPreference[i][j] = file.nextInt();
-				System.out.println(womensPreference[i][j]);
 			}		
 		}
 		
-		//Populate the given match array.
-		System.out.println("Matching is");
+		//Populate the given mens matching.
 		for(int i = 0; i < numPersons; i++) {
 			mMatch[i] = file.nextInt();
-			System.out.println(mMatch[i]);
 		}
 		
-		//populate womans matching
-		for(int i = 0; i < numPersons; i++)
+		//Calculate womens matching.
+		for(int i = 0; i < numPersons; i++) {
 			wMatch[mMatch[i] - 1] = i + 1;
+		}
 		
-		//print womens matching list
-		System.out.println("Womens Matching");
-		for(int i = 0; i < numPersons; i++)
-			System.out.println(wMatch[i]);
-
-		//Preprocess preference lists.
-		System.out.println("Mens Processed List");
+		//Re-process mens preference lists by rank. This step maintains O(n^2) solution.
 		for(int i = 0; i < numPersons; i++) {
 			for(int j = 0; j < numPersons; j++) {
 				mensProcessedList[i][mensPreference[i][j]-1] = j+1;
 			}
 		}
-		for(int i = 0; i < numPersons; i++) {
-			for(int j = 0; j < numPersons; j++) {
-				System.out.println(mensProcessedList[i][j]);
-			}
-		}
-		System.out.println("Womens Processed List");
+		
+		//Re-process womens preference lists by rank. This step maintains O(n^2) solution.
 		for(int i = 0; i < numPersons; i++) {
 			for(int j = 0; j < numPersons; j++) {
 				womensProcessedList[i][womensPreference[i][j]-1] = j+1;
-				//System.out.println(womensProcessedList[i][womensPreference[i][j]-1]);
 			}
 		}
-		//Print processed preference list
-		for(int i = 0; i < numPersons; i++) {
-			for(int j = 0; j < numPersons; j++) {
-				System.out.println(womensProcessedList[i][j]);
-			}
-		}
+		
 		//Check for instability.
-		for(int i = 0; i < numPersons; i++) {
-			for(int j = 0; j < numPersons; j++) {
-				if(mensPreference[i][j] == mMatch[i])
-					break;
-				else if(mensPreference[i][j] < mMatch[i]) {
-					if(womensProcessedList[mensPreference[i][j] - 1][i] < womensProcessedList[mensPreference[i][j] - 1][wMatch[mensPreference[i][j]]])
-						System.out.println("No" + '(' + (i + 1) + ',' + mensPreference[i][j] + ')');
-			}else{
-				System.out.println("Yes");
+		boolean mStable = true;	//Does man prefer his match?
+		boolean wStable = true;	//Does woman prefer her match?
+		int sadMan = -1; 		//index 0 of instability.
+		int sadWoman = -1; 		//index 1 of instability.
+		for(int i = 0; i < numPersons; i++) {//For every man...
+			if(mStable && wStable) {//Guard: Still looking for an instability?
+				for(int j = 0; j < numPersons; j++) {
+					if(mensPreference[i][j] == mMatch[i]) {//Guard: Does man have his preferred woman?
+						break;
+					}
+					if(mensPreference[i][j] != mMatch[i]) {//Does man prefer a woman other than his current match?
+						//local scope variables for readability.
+						int preference = mensPreference[i][j];
+						mStable = false;
+						sadMan = (i + 1);
+						if(womensProcessedList[preference - 1][i] < wMatch[i]) {
+							wStable = false;
+							sadWoman = preference;
+						}
+					}
+				}
 			}
 		}
+		//Print result.
+		if(!mStable && !wStable) {
+			System.out.println("No " + '(' + sadMan + ',' + sadWoman + ')');
+		}else {
+			System.out.println("Yes");
+		}
+		//Close the scanner.
+		file.close();
 	}
-}
 }
